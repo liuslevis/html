1dac2ab1510f9eae98d1c55f714a3e2b3759450b
richliu(刘笑江) <richliu@tencent.com>
2017-03-28 16:09:14 +0800

    refractor

--- html.py
+++ html.py
@@ -7,43 +7,45 @@ def sigmoid(k):
     return 1.0 / (1 + np.exp(-k))
 
 # return scala
-def lr(X,w):
-    # if y==0:
-    #     return sigmoid(X.dot(w))
-    # if y==1:
-    #     return 1.0 - sigmoid(X.dot(w))
-    return 1 - sigmoid(X.dot(w))
+class LR:
+    def f(X,w):
+        return 1 - sigmoid(X.dot(w))
 
-def lr_loss(X,w,Y):
-    return abs(sum(Y.dot(X.dot(w)) - np.log(1+np.exp(X.dot(w))))) + np.linalg.norm(w, 2)
+    def loss(X,w,Y):
+        return abs(sum(Y.dot(X.dot(w)) - np.log(1+np.exp(X.dot(w))))) + np.linalg.norm(w, 2)
 
-# return w.shape
-def lr_grad(X,w,Y=0):
-    grad = np.zeros(w.shape)
-    for i in range(X.shape[0]):
-        x = X[i]
-        y = Y[i]
-        grad += y * x - 1 / (1 + np.exp(x * w)) * x
-    return grad + 0.5 * w
+    # return w.shape
+    def grad(X,w,Y=0):
+        grad = np.zeros(w.shape)
+        for i in range(X.shape[0]):
+            x = X[i]
+            y = Y[i]
+            grad += y * x - 1 / (1 + np.exp(x * w)) * x
+        return grad + 0.5 * w
 
-def lm(X,w):
-    return X.dot(w)
+class LM:
+    def f(X,w):
+        return X.dot(w)
 
-def lm_loss(X,w,Y):
-    n = X.shape[0]
-    return sum((X.dot(w) - Y) ** 2) / n
+    def loss(X,w,Y):
+        n = X.shape[0]
+        return sum((X.dot(w) - Y) ** 2) / n
 
-def lm_grad(X,w,Y):
-    n = X.shape[0]
-    return sum(X * 2) / n
+    def grad(X,w,Y):
+        n = X.shape[0]
+        return sum(X * 2) / n
 
 def err_rate(f,X,w,Y,threshold=0.5):
     return 1.0 - sum(threshold + f(X,w) > Y) / len(Y)
 
-def gd(f,f_grad,f_loss,X,Y,w,learn_rate=0.1,stop_err=0.1, max_iter=20, print_iter=10, classification=True):
+def gd(model,X,Y,w,learn_rate=0.1,stop_err=0.1, max_iter=20, print_iter=10, classification=True):
     assert X.shape[0]==Y.shape[0],'X Y shape diff'
     assert X.shape[1]==w.shape[0],'X w shape diff'
     
+    f = model.f
+    f_grad = model.grad
+    f_loss = model.loss
+
     print('iter\tloss')
     cnt = 0
     while(cnt < max_iter):
@@ -71,9 +73,7 @@ labels += [0 for i in range(100)]
 labels += [1 for i in range(100)]
 
 print("\n========== Linear Regression ==========")
-w = gd(lr,
-    lr_grad,
-    lr_loss,
+w = gd(LR,
     X=np.array(points),
     Y=np.array(labels),
     w=np.array([random(),random(),1]),
@@ -84,9 +84,7 @@ w = gd(lr,
     classification=True)
 
 print("\n========== Linear Models ==========")
-w = gd(lm,
-    lm_grad,
-    lm_loss,
+w = gd(LM,
     X=np.array([[0.1,0.2,1],[0.3,0.4,1],[0.5,0.6,1], [0.6,0.7,1]]),
     Y=np.array([0.1, 0.1, 0.2, 0.3]),
     w=np.array([5,10,10]),
