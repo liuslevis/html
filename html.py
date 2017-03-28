71b9857a4dd4e29091bd520452457b6ff1171de7
richliu(刘笑江) <richliu@tencent.com>
2017-03-28 22:29:07 +0800

    lr sgd gd use iris

--- html.py
+++ html.py
@@ -2,7 +2,8 @@ from math import *
 import numpy as np
 from numpy import transpose as trans
 from random import random
-# np.array([0,1,2]) * np.array([1,2,3])
+from sklearn import datasets
+
 
 def sigmoid(k):
     return 1.0 / (1 + np.exp(-k))
@@ -43,8 +44,7 @@ class LR(Model):
         for i in range(n):
             for j in range(n):
                 W[i][j] = LR.f(X[i],w) * (1 - LR.f(X[j],w))
-        print('rich', trans(X).shape, W.shape, X.shape)
-        H = - trans(X) * W * X
+        H = - np.dot(np.dot(X.T, W), X)
         return H
 
 class LM(Model):
@@ -71,7 +71,7 @@ class Opt:
         assert X.shape[0]==Y.shape[0],'X Y shape diff'
         assert X.shape[1]==w.shape[0],'X w shape diff'
         assert batch <= X.shape[0]
-        print('iter\tloss\terror')
+        print('iter\tloss\ttrain error')
         cnt = 0
         n = X.shape[0]
         while(cnt < max_iter):
@@ -97,7 +97,7 @@ class Opt:
             if train_err < stop_err:
                 print('#%d\t%.2f\t w%s' % (cnt, train_err, w))
                 if classification:
-                    print('train error rate:%.2f' % Opt.err_rate(Model.f, X, w, Y, threshold=0.5))
+                    print('train error rate:%.2f' % train_err)
                 return w
             else:
                 #print('grad w', grad.shape, w.shape)
@@ -108,6 +108,9 @@ class Opt:
         return Opt.sgd(Model,X,Y,w,batch, learn_rate,stop_err, max_iter, print_iter, classification)
 
     def newton(Model,X,Y,w,learn_rate,stop_err,max_iter=20,print_iter=20,classification=True):
+        assert X.shape[0]==Y.shape[0],'X Y shape diff'
+        assert X.shape[1]==w.shape[0],'X w shape diff'
+        print('iter\tloss\ttrain error')
         cnt = 0
         while(cnt < max_iter):
             loss = Model.loss(X,w,Y)
@@ -115,13 +118,25 @@ class Opt:
             H = Model.hessian(X,w,Y)
             H_inv = np.linalg.inv(H)
             w = w - H_inv.dot(g)
+            train_err = Opt.err_rate(Model.f, X, w, Y, threshold=0.5)
+            if cnt % print_iter == 0:
+                print('#%d\t%.2f\t%.2f' % (cnt, loss, train_err))
+            cnt += 1
+            if train_err < stop_err:
+                print('train error rate:%.2f' % train_err)
+                return w
 
-            print('iter\tloss')
-            if cnt % print_iter:
-                print('#%d\t%.2f' % (cnt, loss))
-            if loss < stop_err:
-                print('train error rate:%.2f' % Opt.err_rate(Model.f, X, w, Y, threshold=0.5))
-        pass
+class Data:
+    def load_iris_X_Y_w():
+        iris = datasets.load_iris()
+        sel = iris.target!=2
+        N = sum(sel)
+        features = len(iris.feature_names) + 1
+        X = np.ones((N, features))
+        X[:, :-1] = iris.data[sel]
+        Y = iris.target[sel]
+        w = np.array([0.3 for i in range(features)])
+        return X,Y,w
 
 points = []
 points += [[0.1 * i * random(), 0.5, 1] for i in range(100)]
@@ -132,28 +147,29 @@ labels += [0 for i in range(100)]
 labels += [1 for i in range(100)]
 
 def main():
+    X,Y,w = Data.load_iris_X_Y_w()
 
     # print("\n========== Linear Regression Newton ==========")
     # w = Opt.newton(LR,
-    #     X=np.array(points),
-    #     Y=np.array(labels),
-    #     w=np.array([random(),random(),1]),
-    #     learn_rate=1e-4,
-    #     stop_err=10,
+    #     X=X,
+    #     Y=Y,
+    #     w=w,
+    #     learn_rate=1e-3,
+    #     stop_err=0.05,
     #     max_iter=100,
-    #     print_iter=5,
+    #     print_iter=1,
     #     classification=True)
     # return 
 
     print("\n========== Linear Regression GD ==========")
     w = Opt.gd(LR,
-        X=np.array(points),
-        Y=np.array(labels),
-        w=np.array([random(),random(),1]),
-        learn_rate=1e-7,
-        stop_err=0.05,
-        max_iter=10000,
-        print_iter=500,
+        X=X,#np.array(points),
+        Y=Y,#np.array(labels),
+        w=w,#np.array([random(),random(),1]),
+        learn_rate=1e-3,
+        stop_err=0.01,
+        max_iter=1000,
+        print_iter=1,
         classification=True)
 
     print("\n========== Linear Regression SGD ==========")
@@ -162,10 +178,10 @@ def main():
         Y=np.array(labels),
         w=np.array([random(),random(),1]),
         batch=50,
-        learn_rate=1e-7,
-        stop_err=0.05,
-        max_iter=10000,
-        print_iter=500,
+        learn_rate=1e-3,
+        stop_err=0.01,
+        max_iter=1000,
+        print_iter=100,
         classification=True)
 
     print("\n========== Linear Models ==========")
